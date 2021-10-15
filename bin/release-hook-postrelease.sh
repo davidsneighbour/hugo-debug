150053fce5c7d2db981ea1adf0c7ced326279827
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-10-15 19:55:02 +0700

    build: optimize build scripts

--- bin/release-hook-postrelease.sh
+++ bin/release-hook-postrelease.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+REQUIRED_TOOLS=(
+  git
+)
+
+for TOOL in "${REQUIRED_TOOLS[@]}"; do
+  if ! command -v "${TOOL}" >/dev/null; then
+    echo "${TOOL} is required... "
+    exit 1
+  fi
+done
 
 git push --follow-tags origin main
--- bin/release-hook-prerelease.sh
+++ bin/release-hook-prerelease.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+REQUIRED_TOOLS=(
+  npm
+)
+
+for TOOL in "${REQUIRED_TOOLS[@]}"; do
+  if ! command -v "${TOOL}" >/dev/null; then
+    echo "${TOOL} is required... "
+    exit 1
+  fi
+done
 
 npm run clean
 npm install
