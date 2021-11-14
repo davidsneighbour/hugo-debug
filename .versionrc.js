58aec716bb773b4931f7ea7ad3c4016a8de61a4d
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-11-14 09:52:18 +0700

    chore(config): configure standard-version to save version number in data folder

--- .versionrc.js
+++ .versionrc.js
@@ -1,2 +1,16 @@
-const defaultStandardVersion = require('@dnb-org/standard-version-config');
-module.exports = defaultStandardVersion;
+const defaultStandardVersion = require("@dnb-org/standard-version-config");
+const localStandardVersion =  {
+  bumpFiles: [
+    ...defaultStandardVersion.bumpFiles,
+    {
+      filename: "data/dnb/debug/build.json",
+      type: "json",
+    },
+  ],
+};
+
+const standardVersion = {
+  ...defaultStandardVersion,
+  ...localStandardVersion,
+};
+module.exports = standardVersion;
