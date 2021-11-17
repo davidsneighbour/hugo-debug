fa099a5f8fc654ce5282d9619c7b69dc6fda9c7d
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-11-17 20:05:47 +0700

    tests(config): update configuration for vscode integration

--- tests/cypress/integration/layouts.spec.js
+++ tests/cypress/integration/layouts.spec.js
@@ -1,6 +1,7 @@
-describe("Checking layouts", () => {
+describe("Checking Homepage", () => {
   it("shows the section layout on layout page", () => {
     cy.visit("/");
     cy.get("h1").should("contain", "GoHugo Debug");
+    cy.clearCookies();
   });
 });
